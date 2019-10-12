4e91ad0d0b3847d01195b6535ef27aa9e8d16917
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-12 13:03:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,8 +33,11 @@ class Dog
       INSERT INTO dogs(name, breed)
       VALUES(?, ?); 
       SQL 
-      
-      
+      DB[:conn].execute(sql, name, breed)
+      @id = DB[:conn].execute('SELECT last_insert_rowid() FROM dogs')[0][0]
+    end
+    self
+  end
       
       
   
