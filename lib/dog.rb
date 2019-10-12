856703f94c335ffe84dc42e2ccfa4400195b324e
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-12 12:59:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,9 +18,9 @@ class Dog
     DB[:conn].execute(sql)
   end
   
-  def self.drop_table 
-    sql = <<-SQL 
-    DROP TABLE IF EXIST dogs;
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE IF EXISTS dogs;
     SQL
     DB[:conn].execute(sql)
   end 
