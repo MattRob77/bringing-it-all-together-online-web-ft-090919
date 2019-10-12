b81179bffb2def8f4db6ea650670afff30f26eb2
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-12 13:21:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -99,12 +99,15 @@ class Dog
     end.first
   end
   
-  def update 
-    sql <<-SQL 
-    UPDATE dogs 
-    SET name = 
-    
-  
+   def update
+    sql = <<-SQL
+    UPDATE dogs
+    SET name = ?, breed = ?, id = ?;
+    SQL
+
+    DB[:conn].execute(sql, name, breed, id)
+  end
+end
     
     
 end 
